95e4e10f60eca640164209bee63fd035accc8671
vrvivek443 <v.r.vivek.23eceb@psvpec.in>
2025-01-28 11:15:51 +0530

    Citation-fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
   redirectUri: 'https://localhost:4200/dashboard',
-  postLogoutRedirectUri: 'https://localhost:4200/memberlogin'
+  postLogoutRedirectUri: 'https://localhost:4200/memberlogin',
+  APIBaseURL: 'http://localhost:8001/api/',
+  APIRootURL: 'http://localhost:8001/'
 };
